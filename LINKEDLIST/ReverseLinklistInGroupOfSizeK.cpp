6c3b3f765ce47e26bc99af97f4da1fac092e56e5
prabhat9403 <prabhatpal940@gmail.com>
2025-07-20 20:53:11 +0530

    comment remove

--- LINKEDLIST/ReverseLinklistInGroupOfSizeK.cpp
+++ LINKEDLIST/ReverseLinklistInGroupOfSizeK.cpp
@@ -84,75 +84,7 @@ Node*kReverse(Node*head,int k){
 
 
 
-// Function to reverse nodes in groups of K
-// Node *kReverse(Node *head, int k)
-// {
-//     // Initialize a temporary
-//     // node to traverse the list
-//     Node *temp = head;
 
-//     // Initialize a pointer to track the
-//     // ;ast node of the previous group
-//     Node *prevLast = NULL;
-
-//     // Traverse through the linked list
-//     while (temp != NULL)
-//     {
-//         // Get the Kth node of the current group
-//         Node *kThNode = getKthNode(temp, k);
-
-//         // If the Kth node is NULL
-//         // (not a complete group)
-//         if (kThNode == NULL)
-//         {
-//             // If there was a previous group,
-//             // link the last node to the current node
-//             if (prevLast)
-//             {
-//                 prevLast->next = temp;
-//             }
-
-//             // Exit the loop
-//             break;
-//         }
-
-//         // Store the next node
-//         // after the Kth node
-//         Node *nextNode = kThNode->next;
-
-//         // Disconnect the Kth node
-//         // to prepare for reversal
-//         kThNode->next = NULL;
-
-//         // Reverse the nodes from
-//         // temp to the Kth node
-//         reverseLinkedList(temp);
-
-//         // Adjust the head if the reversal
-//         // starts from the head
-//         if (temp == head)
-//         {
-//             head = kThNode;
-//         }
-//         else
-//         {
-//             // Link the last node of the previous
-//             // group to the reversed group
-//             prevLast->next = kThNode;
-//         }
-
-//         // Update the pointer to the
-//         // last node of the previous group
-//         prevLast = temp;
-
-//         // Move to the next group
-//         temp = nextNode;
-//     }
-
-//     // Return the head of the
-//     // modified linked list
-//     return head;
-// }
 
 int main()
 {
