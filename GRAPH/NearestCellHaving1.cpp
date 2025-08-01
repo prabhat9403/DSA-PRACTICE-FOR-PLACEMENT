682715b3947cacdbcfe668534c9a29f69fd12229
prabhat9403 <prabhatpal940@gmail.com>
2025-08-01 10:57:41 +0530

    nearestcell having 1 question is done

--- GRAPH/NearestCellHaving1.cpp
+++ GRAPH/NearestCellHaving1.cpp
@@ -1,15 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-void find()
-{
-  
-}
 
+int delrow[] = {-1, 0, +1, 0};
+int delcol[] = {0, 1, 0, -1};
+
+void find( int n, int m, vector<vector<int>> &grid, vector<vector<int>> &vis, queue<pair<pair<int, int>, int>>& q, vector<vector<int>>& dist)
+{
+
+  while (!q.empty())
+  {
+    int row = q.front().first.first;
+    int col = q.front().first.second;
+    int dis = q.front().second;
+
+    q.pop();
+
+    for (int i = 0; i < 4; i++)
+    {
+      int nr = row + delrow[i];
+      int nc = col + delcol[i];
+
+      if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 0 && !vis[nr][nc])
+      {
+        dist[nr][nc] = dis + 1;
+        vis[nr][nc]=1;
+        q.push({{nr, nc}, dis + 1});
+      }
+    }
+  }
+}
 int main()
 {
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> grid(n, vector<int>(m, 0));
+  vector<vector<int>> grid(n, vector<int>(m));
+  vector<vector<int>> vis(n, vector<int>(m, 0));
+  vector<vector<int>> dist(n, vector<int>(m, 0));
+
+  queue<pair<pair<int, int>, int>> q;
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < m; j++)
@@ -17,5 +45,31 @@ int main()
       cin >> grid[i][j];
     }
   }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (grid[i][j] == 1)
+      {
+        vis[i][j] = 1;
+        dist[i][j] = 0;
+        q.push({{i, j}, 0});
+      }
+      else{
+        dist[i][j]=20;
+      }
+    }
+  }
+  find(n,m,grid,vis,q,dist);
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout<<dist[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+  
+
   return 0;
 }
